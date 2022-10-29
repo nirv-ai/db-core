63a3722853c7cebfdb0112d4f97f675e366e7d1a
noahehall <noahehall@users.noreply.github.com>
2022-10-29 05:22:43 -0700

    move fixtures to entrypoint, create & hydrate paths table with 325 paths

--- src/docker-entrypoint-initdb.d/001-0-players-schema.sh
+++ src/docker-entrypoint-initdb.d/001-0-players-schema.sh
@@ -3,16 +3,26 @@
 set -e
 
 TABLE_NAME=players
+TABLE_COMMENT='players are users that have not requested account deletion'
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEFAULT_DB" <<-EOSQL
   CREATE TABLE IF NOT EXISTS $DEFAULT_DB.$TABLE_NAME (
     callsign text PRIMARY KEY,
     email text NOT NULL UNIQUE,
     password text NOT NULL,
-    first text,
-    avatar text,
-    about text,
-    last text
+    first text DEFAULT '',
+    avatar text DEFAULT '',
+    about text DEFAULT '',
+    last text DEFAULT ''
   );
+
+  comment on table $TABLE_NAME is '$TABLE_COMMENT';
+  comment on column $TABLE_NAME.callsign is 'players name';
+  comment on column $TABLE_NAME.email is 'players email';
+  comment on column $TABLE_NAME.password is 'players password, PLZ fkn use pgcrypto when modifying';
+  comment on column $TABLE_NAME.avatar is 'a url to an image';
+  comment on column $TABLE_NAME.first is 'players first name';
+  comment on column $TABLE_NAME.last is 'players last name';
+  comment on column $TABLE_NAME.about is 'players about me description';
 
 EOSQL
---
+++ src/docker-entrypoint-initdb.d/002-1-paths-hydrate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## upon errors (as usual) I would totally recommend pgloader
+## ^ to first get data into the db, then you can export for programmatic hydration
+## @see https://pgloader.readthedocs.io/en/latest/quickstart.html
+## sudo apt install pgloader
+## pgloader paths_csv_cmds.load
+
+set -e
+
+TABLE_NAME=paths
+THIS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+TABLE_DATA_FILE="$THIS_DIR/fixtures/paths.csv"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEFAULT_DB" <<-EOSQL
+  COPY $TABLE_NAME
+  FROM '$TABLE_DATA_FILE'
+  CSV HEADER
+  ;
+EOSQL
