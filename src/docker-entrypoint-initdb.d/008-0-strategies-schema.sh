8649f4b8db9874ef8e0a300926b72ab13b4081bc
noahehall <noahehall@users.noreply.github.com>
2022-11-21 18:19:03 -0700

    add strategies schema and convert players_paths to paths_players

---
+++ src/docker-entrypoint-initdb.d/008-0-strategies-schema.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+TABLE_NAME=strategies
+TABLE_COMMENT='strategies are required to excel on paths'
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEFAULT_DB" <<-EOSQL
+  CREATE TABLE IF NOT EXISTS $DEFAULT_DB.$TABLE_NAME (
+    created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+    about text DEFAULT '',
+    name text PRIMARY KEY,
+    created_by text references players(callsign),
+    path text references paths(name),
+    display_name text DEFAULT ''
+  );
+
+  comment on table $TABLE_NAME is '$TABLE_COMMENT';
+  comment on column $TABLE_NAME.name is 'strategies name, once created CANNOT be changed';
+  comment on column $TABLE_NAME.about is 'description of strategy';
+  comment on column $TABLE_NAME.created_by is 'player that created the strategy, nirvai by default';
+  comment on column $TABLE_NAME.display_name is 'a secondary name, as we dont allow changing strategies.name';
+
+EOSQL
--- src/docker-entrypoint-initdb.d/010-0-paths-players-schema.sh
+++ src/docker-entrypoint-initdb.d/010-0-paths-players-schema.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-TABLE_NAME=players_paths
-TABLE_COMMENT='junction table between players and paths'
+TABLE_NAME=paths_players
+TABLE_COMMENT='junction table between paths and players'
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEFAULT_DB" <<-EOSQL
   CREATE TABLE IF NOT EXISTS $DEFAULT_DB.$TABLE_NAME (
@@ -11,11 +11,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEFAULT_DB" <<-EO
     player_callsign TEXT REFERENCES nirvai.players (callsign) ON UPDATE CASCADE ON DELETE CASCADE,
     path_name TEXT REFERENCES nirvai.paths (name) ON UPDATE CASCADE ON DELETE CASCADE,
 
-    CONSTRAINT players_paths_pkey PRIMARY KEY (player_callsign, path_name)
+    CONSTRAINT paths_players_pkey PRIMARY KEY (player_callsign, path_name)
   );
 
   comment on table $TABLE_NAME is '$TABLE_COMMENT';
 
-  CREATE INDEX players_paths_player_callsign_index on nirvai.players_paths (player_callsign) INCLUDE (player_callsign);
-  CREATE INDEX players_paths_path_name_index on nirvai.players_paths (path_name) INCLUDE (path_name);
+  CREATE INDEX paths_players_player_callsign_index on nirvai.paths_players (player_callsign) INCLUDE (player_callsign);
+  CREATE INDEX paths_players_path_name_index on nirvai.paths_players (path_name) INCLUDE (path_name);
 EOSQL
